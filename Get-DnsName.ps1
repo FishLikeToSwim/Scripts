c0041c0b6ec2bffc4c454a32dfbd672152bc5878
FishLikeToSwim <wojciechpietrzak@yahoo.com>
2017-12-10 19:15:12 +0100

    #2 added based help and function

---
+++ Get-DnsName.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+
+
+function Get-DnsName {
+    param (
+
+    )
+    begin {
+
+    }
+    process {
+
+    }
+    end {
+        
+    }
+}
