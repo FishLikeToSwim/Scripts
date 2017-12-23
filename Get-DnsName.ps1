b9cfa9a0045805eb441797d1b87a9427f8e07371
FishLikeToSwim <wojciechpietrzak@yahoo.com>
2017-12-23 21:15:30 +0100

    #5 end

--- Get-DnsName.ps1
+++ Get-DnsName.ps1
@@ -17,23 +17,24 @@
 
 function Get-DnsName {
     param (
-        [Parameter(Mandatory = $false)]
-        [string]$path,
-        [Parameter(Mandatory = $false)]
-        [regex]$regex
+
     )
     begin {
         [string]$path = 'C:\log.txt'
         [regex]$regex = '\b(?:\d{1,3}\.){3}\d{1,3}\b'
     }
     process {
-        
-        $occurences = Select-String -Path $path -Pattern $regex -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value } | ForEach-Object {[System.Net.Dns]::GetHostEntry($_)} 
-
+        $occurences = Select-String -Path $path -Pattern $regex -AllMatches | ForEach-Object { $_.Matches } | ForEach-Object { $_.Value }
         foreach ($o in $occurences) {
-            bierzesz plik, szukasz matchu i podmieniasz
-
-        }
+            try {
+            $hostname = [System.Net.Dns]::GetHostEntry($o) 
+            } catch {
+                $hostname = "Host $o is unreachable"
+                (Get-Content -Path $path).replace("$o", "$hostname") | Set-Content -Path $path
+            }
+            $hostname = $hostname.HostName
+            (Get-Content -Path $path).replace("$o", "$hostname") | Set-Content -Path $path
+        }   
     }
     end {
         
