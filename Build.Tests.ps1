7b841c265bcb6a096632914680addbbefdd80662
PowerShellPlanet <wojciechpietrzak@yahoo.com>
2020-02-12 20:22:34 +0100

    import module

--- Build.Tests.ps1
+++ Build.Tests.ps1
@@ -1,4 +1,5 @@
-
+Install-Module Pester -RequiredVersion 4.10.1
+Import-Module Pester -RequiredVersion 4.10.1
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 <#
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
