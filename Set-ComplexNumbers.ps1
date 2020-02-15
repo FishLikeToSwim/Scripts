b7dd843b49cadabcf831daeb2fd168f80862b1d0
PowerShellPlanet <wojciechpietrzak@yahoo.com>
2020-02-15 16:46:05 +0100

    modified on test purposes

--- Set-ComplexNumbers.ps1
+++ Set-ComplexNumbers.ps1
@@ -1,4 +1,18 @@
-
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    Wojtek 2020
+#>
 function Set-ComplexNumbers {
     [cmdletbinding()]
     param (
